7c4fdf7164c9ccb47e2d63635e41103852810f91
ervnjmsdnts <earvinjamesdantes@gmail.com>
2024-04-22 00:56:11 +0800

    Flashcard update and delete integration

---
+++ src/app/(dashboard)/flashcards/[id]/page.tsx
@@ -0,0 +1,223 @@
+'use client';
+import FullPageScroll from '@/components/full-page-scroll';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { ArrowLeft, Loader2, Plus, X } from 'lucide-react';
+import { useParams, useRouter } from 'next/navigation';
+import { Controller, useFieldArray, useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { db } from '@/firebase';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { useToast } from '@/components/ui/use-toast';
+
+const schema = z.object({
+  name: z
+    .string({ required_error: 'Field is required' })
+    .min(1, 'Field is required'),
+  questions: z
+    .object({
+      question: z
+        .string({ required_error: 'Question is required' })
+        .min(1, 'Field is required'),
+      answer: z
+        .string({ required_error: 'Answer is required' })
+        .min(1, 'Field is required'),
+      difficulty: z
+        .string({ required_error: 'Difficulty is required' })
+        .min(1, 'Difficulty is required'),
+    })
+    .array()
+    .min(1),
+});
+
+type Schema = z.infer<typeof schema>;
+
+type Flashcards = {
+  id: string;
+  name: string;
+  questions: {
+    question: string;
+    answer: string;
+    difficulty: string;
+  }[];
+};
+
+export default function UpdateFlashcardPage() {
+  const params = useParams<{ id: string }>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentFlashcard, setCurrentFlashcard] = useState<Flashcards>(
+    {} as Flashcards,
+  );
+  const router = useRouter();
+
+  const form = useForm<Schema>({
+    resolver: zodResolver(schema),
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+    defaultValues: {
+      name: '',
+      questions: [{ question: '', answer: '', difficulty: '' }],
+    },
+  });
+
+  useEffect(() => {
+    (async () => {
+      const docs = await getDoc(doc(db, 'flashcards', params.id));
+
+      if (docs.exists()) {
+        const data = docs.data() as Flashcards;
+        setCurrentFlashcard(data);
+        form.reset({
+          name: data.name,
+          questions: data.questions,
+        });
+      }
+    })();
+  }, [params.id, form]);
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: 'questions',
+  });
+
+  const { toast } = useToast();
+
+  const onSubmit = async (data: Schema) => {
+    try {
+      setIsLoading(true);
+      await updateDoc(doc(db, 'flashcards', params.id), data);
+      toast({ title: 'Successfully updated Flashcard' });
+      router.back();
+    } catch (_) {
+      toast({
+        title: 'Something went wrong updating flashcard',
+        variant: 'destructive',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const addCard = () => {
+    append({ answer: '', question: '', difficulty: 'easy' });
+    form.trigger('questions');
+  };
+
+  return (
+    <div className='flex flex-col h-full gap-4'>
+      <div className='flex gap-2 items-center'>
+        <Button size='icon' variant='ghost' onClick={() => router.back()}>
+          <ArrowLeft />
+        </Button>
+        <h2 className='text-2xl font-bold'>
+          Update Flashcard ({currentFlashcard.name})
+        </h2>
+      </div>
+      <FullPageScroll>
+        <div className='flex flex-col px-2 max-w-md gap-4'>
+          <div className='grid gap-1.5 relative'>
+            <Label htmlFor='name'>Name</Label>
+            <Input id='name' {...form.register('name')} />
+            <span className='text-destructive text-sm'>
+              {form.formState.errors.name?.message}
+            </span>
+          </div>
+          {fields.map((item, index) => (
+            <div key={item.id}>
+              <div className='flex items-center justify-between pb-4 gap-2'>
+                <p className='font-semibold'>Card {index + 1}</p>
+                {fields.length > 1 && (
+                  <Button
+                    type='button'
+                    onClick={() => remove(index)}
+                    size='icon'
+                    className='p-0 h-6 w-6'
+                    variant='destructive'>
+                    <X className='w-4 h-4' />
+                  </Button>
+                )}
+              </div>
+              <div className='grid gap-4'>
+                <div className='grid gap-1.5'>
+                  <Label htmlFor={`question-${index}`}>Question</Label>
+                  <Input
+                    id={`question-${index}`}
+                    {...form.register(`questions.${index}.question`)}
+                  />
+                  <span className='text-destructive text-sm'>
+                    {
+                      form.formState.errors.questions?.[index]?.question
+                        ?.message
+                    }
+                  </span>
+                </div>
+                <div className='grid gap-1.5'>
+                  <Label htmlFor={`answer-${index}`}>Answer</Label>
+                  <Input
+                    id={`answer-${index}`}
+                    {...form.register(`questions.${index}.answer`)}
+                  />
+                  <span className='text-destructive text-sm'>
+                    {form.formState.errors.questions?.[index]?.answer?.message}
+                  </span>
+                </div>
+                <div className='grid gap-1.5'>
+                  <Label htmlFor={`difficulty-${index}`}>Difficulty</Label>
+                  <Controller
+                    control={form.control}
+                    name={`questions.${index}.difficulty`}
+                    render={({ field }) => (
+                      <div className='grid gap-1.5'>
+                        <Select
+                          onValueChange={field.onChange}
+                          defaultValue={field.value}>
+                          <SelectTrigger>
+                            <SelectValue />
+                          </SelectTrigger>
+                          <SelectContent>
+                            <SelectItem value='easy'>Easy</SelectItem>
+                            <SelectItem value='medium'>Medium</SelectItem>
+                            <SelectItem value='hard'>Hard</SelectItem>
+                          </SelectContent>
+                        </Select>
+                        <span className='text-destructive text-sm'>
+                          {
+                            form.formState.errors.questions?.[index]?.difficulty
+                              ?.message
+                          }
+                        </span>
+                      </div>
+                    )}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+          <div className='items-center flex justify-between'>
+            <Button type='button' variant='outline' onClick={addCard}>
+              Add Question
+              <Plus className='w-4 h-4 ml-2' />
+            </Button>
+            <Button
+              disabled={isLoading}
+              onClick={form.handleSubmit(onSubmit)}
+              type='submit'>
+              {isLoading && <Loader2 className='w-4 h-4 mr-2 animate-spin' />}{' '}
+              Update
+            </Button>
+          </div>
+        </div>
+      </FullPageScroll>
+    </div>
+  );
+}
--- src/app/(dashboard)/flashcards/page.tsx
+++ src/app/(dashboard)/flashcards/page.tsx
@@ -1,53 +1,152 @@
-import { Badge } from '@/components/ui/badge';
+'use client';
+
 import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import { CirclePlus, EllipsisVertical, Pencil, Trash } from 'lucide-react';
+import { useToast } from '@/components/ui/use-toast';
+import { db } from '@/firebase';
+import { collection, deleteDoc, doc, onSnapshot } from 'firebase/firestore';
+import {
+  CirclePlus,
+  EllipsisVertical,
+  Loader2,
+  Pencil,
+  Trash,
+} from 'lucide-react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+type Flashcards = {
+  id: string;
+  name: string;
+  questions: {
+    question: string;
+    answer: string;
+    difficulty: string;
+  }[];
+};
 
 export default function FlashcardsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [flashcards, setFlashcards] = useState<Flashcards[]>([]);
+
+  const router = useRouter();
+
+  const { toast } = useToast();
+
+  useEffect(() => {
+    (() => {
+      const unsub = onSnapshot(collection(db, 'flashcards'), (snapshot) => {
+        const flashcards = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          return { ...data, id: doc.id };
+        });
+        setFlashcards(flashcards as Flashcards[]);
+        setIsLoading(false);
+      });
+
+      return () => {
+        unsub();
+      };
+    })();
+  }, []);
+
+  const deleteFlashcard = async (id: string) => {
+    const ref = doc(db, 'flashcards', id);
+
+    try {
+      await deleteDoc(ref);
+      router.refresh();
+      toast({ title: 'Successfully deleted flashcard' });
+    } catch (_) {
+      toast({ title: 'Something went wrong when deleting flashcard' });
+    }
+  };
+
   return (
     <div className='flex flex-col gap-4 w-full'>
       <div className='flex items-center justify-between'>
         <h2 className='text-2xl font-bold'>Flashcards</h2>
-        <Button asChild>
+        <Button asChild disabled={isLoading}>
           <Link href='/flashcards/add'>
             <CirclePlus className='mr-2 w-5 h-5 text-white' />
             Add Flashcard
           </Link>
         </Button>
       </div>
-      <div className='grid grid-cols-4 gap-3 w-full'>
-        {new Array(6).fill(null).map((_, index) => (
-          <div key={index} className='border rounded-lg bg-white p-4'>
-            <div className='flex gap-2 justify-between'>
-              <h3 className='font-semibold'>Name</h3>
-              <DropdownMenu>
-                <DropdownMenuTrigger className=''>
-                  <EllipsisVertical className='w-5 h-5' />
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem>
-                    <Pencil className='w-4 mr-2 h-4' /> Edit
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <Trash className='mr-2 w-4 h-4' />
-                    Delete
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
+      {isLoading ? (
+        <div className='justify-center items-center flex'>
+          <Loader2 className='w-6 h-6 animate-spin' />
+        </div>
+      ) : (
+        <div className='grid grid-cols-4 gap-3 w-full'>
+          {flashcards.map((flashcard) => (
+            <div key={flashcard.id} className='border rounded-lg bg-white p-4'>
+              <div className='flex gap-2 justify-between'>
+                <h3 className='font-semibold'>{flashcard.name}</h3>
+                <Dialog>
+                  <DropdownMenu>
+                    <DropdownMenuTrigger className=''>
+                      <EllipsisVertical className='w-5 h-5' />
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                      <DropdownMenuItem
+                        onClick={() =>
+                          router.push(`/flashcards/${flashcard.id}`)
+                        }>
+                        <Pencil className='w-4 mr-2 h-4' /> Edit
+                      </DropdownMenuItem>
+                      <DialogTrigger className='flex items-center w-full'>
+                        <DropdownMenuItem className='w-full'>
+                          <Trash className='mr-2 w-4 h-4' />
+                          Delete
+                        </DropdownMenuItem>
+                      </DialogTrigger>
+                    </DropdownMenuContent>
+                  </DropdownMenu>
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Are you sure?</DialogTitle>
+                      <DialogDescription>
+                        Do you want to delete the flashcard? Deleting this
+                        flashcard cannot be undone.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter>
+                      <DialogClose asChild>
+                        <Button variant='outline'>Cancel</Button>
+                      </DialogClose>
+                      <Button
+                        variant='destructive'
+                        onClick={() => deleteFlashcard(flashcard.id)}>
+                        Delete
+                      </Button>
+                    </DialogFooter>
+                  </DialogContent>
+                </Dialog>
+              </div>
+              <p className='text-sm text-muted-foreground'>
+                Questions: {flashcard.questions.length}
+              </p>
             </div>
-            <div className='flex justify-between items-center pt-2'>
-              <p className='text-sm text-muted-foreground'>Questions: 2</p>
-              <Badge>Easy</Badge>
-            </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
