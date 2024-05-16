c24ed4facf0ad711529551edf0d0bf3c37e9cb88
ervnjmsdnts <earvinjamesdantes@gmail.com>
2024-05-16 12:32:37 +0800

    Show request

--- src/app/api/consumer/route.ts
+++ src/app/api/consumer/route.ts
@@ -2,10 +2,14 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
   try {
+    console.log('REQUEST: ', request);
     const test = await request.text();
     console.log('RESULTS: ', test);
 
-    return NextResponse.json({ message: 'Success' }, { status: 200 });
+    return NextResponse.json(
+      { message: 'Success', body: test },
+      { status: 200 },
+    );
   } catch (error) {
     return NextResponse.json(
       { message: 'Something went wrong' },
